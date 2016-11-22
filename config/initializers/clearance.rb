53235261b624177f9a175827fd3d18910f263073
aimensasi <ali.sasi@ymail.com>
2016-11-22 14:57:04 +0800

    added clearance layout

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,6 @@
 Clearance.configure do |config|
   config.mailer_sender = "reply@example.com"
 end
+
+Clearance::SessionsController.layout "logged_out"
+Clearance::UsersController.layout "logged_out"
