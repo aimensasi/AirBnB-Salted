df5c5913a0b2103896715f518e07c0b07de47e97
aimensasi <ali.sasi@ymail.com>
2016-11-22 15:10:06 +0800

    added figaro

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   provider :facebook, '344398932588275', 'dbca76c3a9de74076d314c19390d3ed9'
+   provider :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"]
 end
