9f7ba3e7188dca7a8c6eb7d6e608453e9ef455a1
aimensasi <ali.sasi@ymail.com>
2016-12-01 10:53:47 +0800

    added braintree payment to server

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,9 +3,11 @@ class TransactionsController < ApplicationController
 	layout 'alt_layout'
 
   def new
+  	@client_token =  Braintree::ClientToken.generate
   end
 
   def create
+  	puts "params From Form :$$$$: >>> #{params}"
   end
 
 end
